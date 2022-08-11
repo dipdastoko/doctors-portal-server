7079a2f675747d6d9ae93e8f604b69445ac2f7dd
dipdastoko <dipdastoko@gmail.com>
2022-08-12 04:06:50 +0600

    database connected

--- index.js
+++ index.js
@@ -12,7 +12,17 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-console.log(uri);
+async function run() {
+    try {
+        await client.connect();
+        console.log('database connected successfully');
+    }
+    finally {
+        // await client.close();
+    }
+
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
