337109f5f92d622d1f0b5070f7f4ef5090542495
dipdastoko <dipdastoko@gmail.com>
2022-08-16 04:58:07 +0600

    get and post api added for appointments

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zqquk.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -15,7 +16,24 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        console.log('database connected successfully');
+
+        const database = client.db('doctors_portal');
+        const appointmentCollection = database.collection('appointments');
+
+        app.get('/appointments', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email }
+            const cursor = appointmentCollection.find(query);
+            const appointments = await cursor.toArray();
+            res.json(appointments);
+        })
+
+        app.post('/appointments', async (req, res) => {
+            const appointment = req.body;
+            const result = await appointmentCollection.insertOne(appointment);
+            console.log(result);
+            res.json(result);
+        });
     }
     finally {
         // await client.close();
